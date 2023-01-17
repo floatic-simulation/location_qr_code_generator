16ac0244e384641bdeef6f6d39625274df9a0454
cooniecoon <hoony3355@gmail.com>
2023-01-17 17:43:26 +0900

    remove unnecessary code
    Signed-off-by: cooniecoon <hoony3355@gmail.com>

--- location_qr_generator.py
+++ location_qr_generator.py
@@ -1,22 +1,16 @@
 import qrcode
 import os
-import pandas as pd
 
-location_code = "A2-01-01-04"
+zone_list = ["A","B","C"]  # list of Zone
+aisle_num = 2  # number of aisles in zone
+rack_num = 4  # number of racks in aisle
+A_level_num = 3  # number of levels in zone A
+BC_level_num = 4  # number of levels in zone B, C
 
-zone_list = ["A","B","C"]
-aisle_num = 2
-rack_num = 4
-A_level_num = 3
-B_level_num = 4
+cell_num = 4  # number of cells in level
 
-cell_num = 4
-
-image_path = "./qr_img"
+image_path = "./qr_img" # path to save image
 abs_path = os.path.abspath(image_path)
-
-location_code_list = []
-qr_path_list = []
 
 qr = qrcode.QRCode(
     version=3,
@@ -28,7 +22,7 @@ for zone in zone_list:
     if zone == "A":
         level_num = A_level_num
     elif zone == "B" or zone == "C":
-        level_num = B_level_num
+        level_num = BC_level_num
     
     for aisle in range(aisle_num):
         for rack in range(rack_num):
@@ -40,10 +34,3 @@ for zone in zone_list:
                     img = qr.make_image(fill_color="black", back_color="white")
                     img.save(f"{abs_path}/{location_code}.png")
                     print(location_code, ".png saved")
-                    location_code_list.append(location_code)
-                    qr_path_list.append(abs_path)
-
-df = pd.DataFrame({'image_path':qr_path_list, 'location_code':location_code_list})
-with pd.ExcelWriter("testbed_location_codes.xlsx") as writer:
-     df.to_excel(writer, sheet_name = 'location_codes')
-
